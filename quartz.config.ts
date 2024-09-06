2506eaa28744207a78869b72d95a338c64ad358e
Mykhailo Ziatin <mikhail.zyatin@gmail.com>
2024-09-06 13:45:42 +0300

    Quartz sync: Sep 6, 2024, 1:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Mykhailo Ziatin",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://mykhailo-ziatin.github.io/_",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
